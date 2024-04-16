17c5a84c78dc92295813d394e938526d0528f600
BjarkeBP <Bjarkebp@live.dk>
2024-04-16 20:12:20 +0200

    added tailwind and fixed some things
    npm install @babel/plugin-proposal-private-property-in-object --save-dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-function App() {
-  return <div>Oof</div>;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-blue-500">Hello world!</h1>
+  );
 }
-
-export default App;
