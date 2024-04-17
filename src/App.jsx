486cde9425538cd00124fccb1dd7104fb36b2975
BjarkeBP <Bjarkebp@live.dk>
2024-04-17 09:41:25 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Scanner from "./comps/scanner";
+import barcodelookup from "./services/barcodelookup";
 
 export default function App() {
   const [scanResult, setScanResult] = useState(null);
 
+  useEffect(() => {
+    if (scanResult) {
+      barcodelookup(scanResult);
+    }
+  }, [scanResult]);
+
+  function test() {
+    barcodelookup("7622300683269");
+  }
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
+      <button onClick={test}>Click me</button>
       <Scanner setScanResult={setScanResult} />
       {scanResult ? <div>Success: {scanResult}</div> : <div></div>}
     </div>
